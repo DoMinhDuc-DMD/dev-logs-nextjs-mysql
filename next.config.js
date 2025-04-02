31445586df245ee798f64eea913e29dc73640e2d
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-04-02 17:40:11 +0700

    small fix next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// import type { NextConfig } from "next";
+/** @type {import{'next'}.nextConfig} **/
+const nextConfig = {
+  /* config options here */
+  output: "export"
+};
+
+module.exports = nextConfig;
