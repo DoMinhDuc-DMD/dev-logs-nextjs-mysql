b62218fa1c930c9150ce4bb8304f158818df9d6d
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-04-02 17:45:54 +0700

    recover old commit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// import type { NextConfig } from "next";
-/** @type {import{'next'}.nextConfig} **/
-const nextConfig = {
-  /* config options here */
-  output: "export"
-};
-
-module.exports = nextConfig;
