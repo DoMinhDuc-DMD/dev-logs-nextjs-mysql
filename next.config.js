40d0b1040eedb79cf6819b46a6499aa61c2ee9a0
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-04-04 15:00:12 +0700

    fix to test deploy 28

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Since this is an auth page that needs dynamic functionality
-  // You might want to exclude it from static export
-  images: {
-    unoptimized: true,
-  },
-  // Optional: Enable trailing slashes if needed
-  trailingSlash: true,
+
 }
 
 export default nextConfig
