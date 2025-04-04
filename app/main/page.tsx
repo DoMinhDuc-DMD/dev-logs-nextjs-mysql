2f0a21ed7dbd8b199d35db69492624d0bba9f4f0
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-04-04 09:42:04 +0700

    fix to test deploy 17

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <div className="p-5 h-[93vh]">
-      <div className="flex h-full rounded bg-white justify-center pt-10 text-2xl">Trang tổng quan hệ thống phần mềm quản lý log làm việc của nhân viên</div>
+      <div className="flex h-full rounded bg-white justify-center pt-10 text-2xl">
+        Trang tổng quan hệ thống phần mềm quản lý log làm việc của nhân viên
+      </div>
     </div>
   );
 }
