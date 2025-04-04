6247ac8d746d8cf849e4cefb552ed1be2a4f3302
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-04-04 11:26:10 +0700

    fix to tét deploy 22

---
+++ app/api/accountDetail/route.tsx
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import dayjs from "dayjs";
+import db from "../connectdb/db";
+import { RowDataPacket } from "mysql2";
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = new URL(req.url).searchParams;
+    const userId = searchParams.get("id");
+
+    const [accounts] = await db.query<RowDataPacket[]>("SELECT * FROM account WHERE id = ?", [userId]);
+
+    const user = accounts[0];
+    if (user.employee_birthday) {
+      user.employee_birthday = dayjs(new Date(user.employee_birthday)).format("DD/MM/YYYY");
+    }
+    if (!accounts || accounts.length === 0) {
+      return NextResponse.json({ message: "Không tìm thấy tài khoản" }, { status: 404 });
+    }
+
+    return NextResponse.json(accounts[0]);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Lỗi server" }, { status: 500 });
+  }
+}
---
+++ app/api/devlogInput/route.tsx
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+import db from "../connectdb/db";
+import { RowDataPacket } from "mysql2";
+
+export async function GET() {
+  try {
+    const [project] = await db.query<RowDataPacket[]>(
+      "SELECT project.id,project.project_name, member_project.account_id FROM project INNER JOIN member_project ON project.id = member_project.project_id"
+    );
+    const [task] = await db.query<RowDataPacket[]>("SELECT * FROM task WHERE isAvailable = true");
+
+    const formattedProject = project.map((row) => ({
+      value: row.id,
+      label: row.project_name,
+      accountId: row.account_id,
+    }));
+
+    const formattedTask = task.map((row) => ({
+      value: row.id,
+      label: row.task_name,
+      projectId: row.project_id,
+    }));
+
+    return NextResponse.json({ formattedProject, formattedTask });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Lỗi server" }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId, hours, overtime, date, note, project, task } = await req.json();
+
+    if (!project || !task || !hours || !date) {
+      return NextResponse.json({ message: "Vui lòng nhập đầy đủ thông tin" }, { status: 400 });
+    }
+
+    await db.query(`INSERT INTO devlog (hours, overtime, date, note, account_id, project_id, task_id) VALUES (?, ?, ?, ?, ?, ?, ?)`, [
+      hours,
+      overtime,
+      date,
+      note,
+      userId,
+      project,
+      task,
+    ]);
+
+    return NextResponse.json({ message: "Thêm devlog thành công!" });
+  } catch (error) {
+    console.error("Lỗi khi lưu dữ liệu:", error);
+    return NextResponse.json({ message: "Lỗi server khi lưu dữ liệu" }, { status: 500 });
+  }
+}
